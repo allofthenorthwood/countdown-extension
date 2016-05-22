11cbd67d4ebc3557182c68a174092f8aac0fa744
Oliver Northwood <oliver@khanacademy.org>
2016-05-22 08:15:54 -0700

    Set up bckground canvas

--- fancy-backgrounds.js
+++ fancy-backgrounds.js
@@ -1,3 +1,16 @@
-(function (exports) {
-    // TODO
+(function(exports) {
+
+    var canvas = document.getElementById("main-background-canvas");
+    var ctx = canvas.getContext("2d");
+    var pixelRatio = 2.0;
+    var canvasWidth = window.innerWidth;
+    var canvasHeight = window.innerHeight;
+    canvas.setAttribute("width", canvasWidth * pixelRatio);
+    canvas.setAttribute("height", canvasHeight * pixelRatio);
+    canvas.style.width = canvasWidth + "px";
+    canvas.style.height = canvasHeight + "px";
+    ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
+
+    exports.Confetti(canvas, ctx, canvasWidth, canvasHeight);
+
 })(globalModules);
