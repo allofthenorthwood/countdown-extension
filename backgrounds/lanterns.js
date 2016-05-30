b58bf359f99778129893adb899c83903794e4427
Oliver Northwood <oliver@khanacademy.org>
2016-05-29 18:49:29 -0700

    Use darkest color as lantern background

--- backgrounds/lanterns.js
+++ backgrounds/lanterns.js
@@ -1,31 +1,91 @@
 (function(exports) {
 
-
 // From http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
-function hexToRgba(hex, a) {
+function hexToRgb(hex) {
   // Expand shorthand form (e.g. '03F') to full form (e.g. '0033FF')
-  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
   hex = hex.replace(shorthandRegex, function(m, r, g, b) {
       return r + r + g + g + b + b;
   });
 
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  if (!result) {
-    return null;
-  }
-  var rgb = [
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? [
     parseInt(result[1], 16),
     parseInt(result[2], 16),
     parseInt(result[3], 16),
-  ];
+  ] : null;
+};
+
+function printHexToRgba(hex, a) {
+  var rgb = hexToRgb(hex);
   return 'rgba(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ',' + a + ')';
 };
+
+/**
+ * Converts an RGB color value to HSL. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and l in the set [0, 1].
+ *
+ * http://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSL representation
+ */
+var rgbToHsl = function(rgb) {
+  var r = rgb[0] / 255;
+  var g = rgb[1] / 255;
+  var b = rgb[2] / 255;
+
+  var max = Math.max(r, g, b);
+  var min = Math.min(r, g, b);
+  let h, s, l = (max + min) / 2;
+
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+
+    h /= 6;
+  }
+
+  return [h, s, l];
+};
+
+var findDarkestColorIdx = function(colors) {
+  var darkest = {
+    idx: 0,
+    lightness: 255,
+  };
+  for (var i = 0; i < colors.length; i++) {
+    var color = colors[i];
+    var lightness = rgbToHsl(hexToRgb(color))[2];
+    if (lightness < darkest.lightness) {
+      darkest = {
+        idx: i,
+        lightness: lightness,
+      };
+    }
+  }
+  return darkest.idx;
+}
 
 exports.Lanterns = function(canvas, colorPalettes) {
   function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   var colors = colorPalettes[randomInt(0, colorPalettes.length - 1)];
+  var backgroundColorIdx = findDarkestColorIdx(colors);
+  backgroundColor = colors[backgroundColorIdx];
+  colors.splice(backgroundColorIdx, 1);
 
   var ctx = canvas.getContext("2d");
   var pixelRatio = 2.0;
@@ -36,7 +96,7 @@ exports.Lanterns = function(canvas, colorPalettes) {
   var lines = [];
 
   var drawStarLantern = function(x, y, size, color) {
-    var shadeColor = "rgba(50, 50, 100, 0.15)";
+    var shadeColor = "rgba(30, 30, 40, 0.15)";
     ctx.save();
     ctx.translate(x,y);
     ctx.scale(size / 100,size / 100);
@@ -122,12 +182,12 @@ exports.Lanterns = function(canvas, colorPalettes) {
   };
 
   var getRandomColor = function() {
-    var color = colors[randomInt(1, colors.length - 1)];
+    var color = colors[randomInt(0, colors.length - 1)];
     return color;
   };
 
   function tick() {
-    ctx.fillStyle = colors[0];
+    ctx.fillStyle = backgroundColor;
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
     for (var i = 0; i < lines.length; i++) {
@@ -165,7 +225,7 @@ exports.Lanterns = function(canvas, colorPalettes) {
         lantern.roost.y + lantern.size / 2,
         0
       );
-      glow.addColorStop(0, hexToRgba(lantern.color, 0));
+      glow.addColorStop(0, printHexToRgba(lantern.color, 0));
       glow.addColorStop(1, lantern.color);
       ctx.fillStyle = glow;
       ctx.globalAlpha = 0.8;
@@ -199,12 +259,12 @@ exports.Lanterns = function(canvas, colorPalettes) {
     // Pick a point on the left side of the screen, and a point on the right
     // side of the screen for the edges of the power line:
     var leftPoint = {
-      x: -100,
-      y: randomInt(clickPoint.y - 100, clickPoint.y - 50),
+      x: -300,
+      y: randomInt(clickPoint.y - 200, clickPoint.y - 50),
     };
     var rightPoint = {
-      x: canvasWidth + 100,
-      y: randomInt(clickPoint.y - 100, clickPoint.y - 50),
+      x: canvasWidth + 300,
+      y: randomInt(clickPoint.y - 200, clickPoint.y - 50),
     };
 
     var controlPoint = {
